01fc4939e76674baa9c113abba5886c3e001beaf
Fernando Luiz Goulart <fernando.goulart@olxbr.com>
2021-03-18 13:58:40 -0300

    remove dead code

--- ebooks/Common/DataManagers/EBookDataManager.swift
+++ ebooks/Common/DataManagers/EBookDataManager.swift
@@ -25,12 +25,6 @@ protocol EBookDataManager: AnyObject {
     func getEBooks(from term: String) -> Promise<EBooks>
 }
 
-extension EBookDataManager {
-    func getEBooks(from term: String, test: Bool  = false) -> Promise<EBooks> {
-        return getEBooks(from: term)
-    }
-}
-
 final class EBookNetworkManager: EBookDataManager {
     func getEBooks(from term: String) -> Promise<EBooks> {
         return APIManager.callApi(APIRouter(route: Route.getBooks(term: term)), dataReturnType: EBooks.self)
--- ebooksTests/Scenes/Listing/ListingInteractorTests.swift
+++ ebooksTests/Scenes/Listing/ListingInteractorTests.swift
@@ -13,10 +13,12 @@ class ListingInteractorTests : XCTestCase {
 
     private var listingInteractor: ListingInteractor?
     private var dataManager : EBookDataManager?
+    private var errorDataManager: EBookDataManager?
 
     override func setUp() {
         listingInteractor = ListingInteractor()
         dataManager = EBookDataManagerMock()
+        errorDataManager = EBookDataManagerMockError()
     }
 
     func testFetchEbooks() {
@@ -33,34 +35,31 @@ class ListingInteractorTests : XCTestCase {
                 eBooks: nil,
                 error: ListingError.couldNotFetchEBooks(error: "test error"))
             XCTAssert(false)
-            expectation.fulfill()
         }.finally {
             XCTAssertEqual(response.eBooks?.count, 50)
             expectation.fulfill()
         }
         waitForExpectations(timeout: 30, handler: nil)
     }
+
+    func testFetchEbooksError() {
+        var response: ListingPage.FetchEBooks.Response!
+        let expectation = self.expectation(description: "Get EBooks Test")
+        self.errorDataManager?.getEBooks(from: "switch").done { eBooks in
+            var newEBooks: [EBook] = []
+            for eBook in eBooks.results ?? [] {
+                newEBooks.append(eBook)
+            }
+            response = ListingPage.FetchEBooks.Response(eBooks: newEBooks, error: nil)
+        }.catch { _ in
+            response = ListingPage.FetchEBooks.Response(
+                eBooks: nil,
+                error: ListingError.couldNotFetchEBooks(error: "test error"))
+            XCTAssert(true)
+        }.finally {
+            XCTAssertNil(response.eBooks)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 30, handler: nil)
+    }
 }
-
-/*
- var presenter: ListingPresentationLogic?
- var worker = ListingWorker()
- var eBooks: EBooks?
-
- func fetchTerm(request: ListingPage.FetchEBooks.Request) {
-     var response: ListingPage.FetchEBooks.Response!
-     self.worker.eBookDataManager.getEBooks(from: request.term).done { eBooks in
-         var newEBooks: [EBook] = []
-         for eBook in eBooks.results ?? [] {
-             newEBooks.append(eBook)
-         }
-         response = ListingPage.FetchEBooks.Response(eBooks: newEBooks, error: nil)
-     }.catch { error in
-         response = ListingPage.FetchEBooks.Response(
-             eBooks: nil,
-             error: ListingError.couldNotFetchEBooks(error: error.localizedDescription))
-     }.finally {
-         self.presenter?.presentListing(response: response)
-     }
- }
- */
